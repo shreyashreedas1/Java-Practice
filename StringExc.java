0940839b6a5a5f4fe2ea63697cf4cafd80112a74
Shreyashree Das <47034394+shreyashreedas1@users.noreply.github.com>
2019-04-02 22:01:24 +0530

    Add files via upload

--- StringExc.java
+++ StringExc.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
-
+ 
 class StringMismatchException extends Exception {
-
-    public StringMismatchException(String str) {
-
-        System.out.println(str);
-    }
+ 
+ public StringMismatchException(String str) {
+  
+  System.out.println(str);
+ }
 }
 public class StringExc {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the string : ");
-        String input = sc.nextLine();
-
-        try {
-            if(input.equalsIgnoreCase("India"))
-                System.out.println("String matched !!!");
-            else
-                throw new StringMismatchException("String is not matched with INDIA");
-        }
-        catch (StringMismatchException e) {
-            System.out.println(e);
-        }
-    }
-
+ 
+ public static void main(String[] args) {
+  
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter the string : ");
+  String input = sc.nextLine();
+  
+  try {
+   if(input.equalsIgnoreCase("India"))
+    System.out.println("String matched !!!");
+   else
+    throw new StringMismatchException("String is not matched with INDIA");
+  }
+  catch (StringMismatchException e) {
+   System.out.println(e);
+  }
+ }
+ 
 }
